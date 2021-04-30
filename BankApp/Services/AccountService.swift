689030ed68c3d507a835d1ec2ff306bfe23e6a0d
elizeurs <13ders@gmail.com>
2021-04-30 19:39:51 -0300

    Creating Project || Implementing Account Service to Get All Accounts

---
+++ BankApp/Services/AccountService.swift
@@ -0,0 +1,43 @@
+//
+//  AccountService.swift
+//  BankApp
+//
+//  Created by Elizeu RS on 30/04/21.
+//  Copyright © 2021 Mohammad Azam. All rights reserved.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+  case badUrl
+  case decodingError
+  case noData
+}
+
+class AccountService {
+  
+  private init() { }
+  
+  static let shared = AccountService()
+  
+  func getAllAccounts(completion: @escaping(Result<[Account]?, NetworkError>) -> Void) {
+    
+    guard let url = URL.urlForAccounts() else {
+      return completion(.failure(.badUrl))
+    }
+    
+    URLSession.shared.dataTask(with: url) { data, response, error in
+      
+      guard let data = data, error == nil else {
+        return completion(.failure(.noData))
+      }
+      
+      let accounts = try? JSONDecoder().decode([Account].self, from: data)
+      if accounts == nil {
+        completion(.failure(.decodingError))
+      } else {
+        completion(.success(accounts))
+      }
+    }.resume()
+  }
+}
