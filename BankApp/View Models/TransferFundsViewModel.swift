a74908de4712b7020d03e419d799895ac8351a21
elizeurs <13ders@gmail.com>
2021-05-04 13:28:14 -0300

    Implementing TransferFundViewModel

---
+++ BankApp/View Models/TransferFundsViewModel.swift
@@ -0,0 +1,33 @@
+//
+//  TransferFundsViewModel.swift
+//  BankApp
+//
+//  Created by Elizeu RS on 04/05/21.
+//  Copyright © 2021 Mohammad Azam. All rights reserved.
+//
+
+import Foundation
+
+class TransferFundsViewModel: ObservableObject {
+  
+  var fromAccount: AccountViewModel?
+  var toAccount: AccountViewModel?
+  
+  @Published var accounts: [AccountViewModel] = [AccountViewModel]()
+  
+  func populateAccounts() {
+    
+    AccountService.shared.getAllAccounts { result in
+      switch result {
+      case .success(let accounts):
+        if let accounts = accounts {
+          DispatchQueue.main.async {
+            self.accounts = accounts.map(AccountViewModel.init)
+          }
+        }
+      case .failure(let error):
+        print(error.localizedDescription)
+      }
+    }
+  }
+}
